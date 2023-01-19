c79e1294129dd5bb17d813fe51c6afa24684ac52
MairaDuqueA <mduquea2@gmail.com>
2023-01-19 18:35:32 -0500

    rutas

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const publicPath = path.resolve(__dirname, './views');
-app.use(express.static(publicPath));
+
+app.use(express.static("public")); 
+app.use(express.static("views"));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, '/views/index.html'));
+    res.sendFile(path.resolve(__dirname, '/index.html'));
 });
 
 app.get('/register', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, '/views/register.html'));
+    res.sendFile(path.resolve(__dirname, '/register.html'));
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, '/login.html'));
 });
 
 app.listen(PORT, () => {
